5e55d1e2e3d275e61d802b53ff098b07a8c40368
Denis Popov <dzutaro@gmail.com>
2019-03-22 22:41:03 +0300

    Initial commit

---
+++ js/perfmatters.js
@@ -0,0 +1,15 @@
+// Measuring the Critical Rendering Path with Navigation Timing
+// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+
+function logCRP() {
+  var t = window.performance.timing,
+      interactive = t.domInteractive - t.domLoading,
+      dcl = t.domContentLoadedEventStart - t.domLoading,
+      complete = t.domComplete - t.domLoading;
+  var stats = document.getElementById("crp-stats");
+  stats.textContent = 'DI:' + interactive + 'ms, DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+}
+
+window.addEventListener("load", function(event) {
+  logCRP();
+});
